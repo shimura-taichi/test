041ebbe5b73a376e8131a14a65d9b4c4f66c4978
shimura-taichi <155545427+shimura-taichi@users.noreply.github.com>
2024-02-26 22:14:32 +0900

    Add files via upload

---
+++ java 演習課題/Person2/src/Person.java
@@ -0,0 +1,33 @@
+//演習①
+public class Person {
+	public String name=null;
+	public int age=0;
+	
+	public Person(){}
+	
+	public Person(String name,int age){
+		this.name=name;
+		this.age=age;
+		
+		
+	}
+	//コンストラクタ③
+	public Person(String name){
+		 this.name = name;
+		 this.age = 0;
+	}
+	
+	//コンストラクタ④
+	public Person(int age){
+		this.name="名前なし";
+		this.age=age;
+	}
+	
+	//コンストラクタ⑤
+	public Person(int age, String name){
+		this.name = name;
+		this.age=age;
+		
+	}
+
+}
---
+++ java 演習課題/Person2/src/Test.java
@@ -0,0 +1,32 @@
+//演習①
+public class Test {
+	public static void main(String[]args){
+
+		Person taro=new Person();
+		taro.name="taro";
+		taro.age=18;
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+
+		Person jiro=new Person("jiro",20);
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+
+		//コンストラクタ③
+		Person saburo=new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+		//コンストラクタ④
+		Person namaenashi=new Person(25);
+		System.out.println(namaenashi.name);
+		System.out.println(namaenashi.age);
+
+		//コンストラクタ⑤
+		Person hanako=new Person(17,"hanako");
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+	}
+
+
+}
